43fdf3607a4b2e6dda1e4e80f8aaaab9efee704e
wijiler <josiahosgood@gmail.com>
2021-08-09 05:07:30 -0400

    ty togglebit

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use std::io::Write;
 fn createfile() -> io::Result<()> {
     let mut filename = String::new();
     println!("Enter file name here :");
-    let _filenameoutput = io::stdin().read_line(&mut filename).unwrap();
+    let _filenameoutput = io::stdin().read_line(&mut filename)?;
     let mut file = File::create(filename)?;
     println!("Wee - the whatever editor\n");
     let mut write = String::new();
-    let _writing = io::stdin().read_line(&mut write).unwrap();
+    let _writing = io::stdin().read_line(&mut write)?;
+    write.pop(); // ty togglebit
     write!(file, "{}", write)?;
     Ok(())
 }
